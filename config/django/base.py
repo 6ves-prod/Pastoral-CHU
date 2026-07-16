98a311e2c4544baf4f0bd91d3bafc5c292bc11fd
Votre Nom <groupfaces0@gmail.com>
2026-07-16 15:54:15 +0000

    feat: deploy render

--- config/django/base.py
+++ config/django/base.py
@@ -21,10 +21,10 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', "Secret")
+SECRET_KEY = env('SECRET_KEY', default="Secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DJANGO_DEBUG', False)
+DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
 ALLOWED_HOSTS = [
     "Pastoralchu.onrender.com",
