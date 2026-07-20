9fd1c9d186ac6123f4183e3931e32f9b72d4a502
Votre Nom <groupfaces0@gmail.com>
2026-07-20 21:58:30 +0000

    feat: deploy render

--- build.sh
+++ build.sh
@@ -8,4 +8,28 @@ python manage.py makemigrations --noinput
 
 python manage.py migrate
 
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+        print("Superuser créé.")
+    else:
+        print("Le superutilisateur existe déjà.")
+else:
+    print("Variables d'environnement du superutilisateur absentes.")
+EOF
+
 python manage.py collectstatic --noinput
